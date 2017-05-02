ce86fc355c87b70b160adcf49a5aec4ad7e2e48e
Saul Flores <saulpolo95@gmail.com>
2017-05-01 21:36:11 -0700

    Added cards and like functionality

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,7 +1,28 @@
 var page = require('page');
 var empty = require('empty-element');
+var template = require('./template');
 
 page('/', function (ctx, next) {
   var main = document.getElementById('main-container');
-  main.innerHTML = '<a href="/signup">Signup</a>'
+  var pictures = [
+    {
+      user: {
+        username: 'saulpolo95',
+        avatar: 'https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-1/c0.5.160.160/p160x160/13872993_174638556288372_5069571747674313849_n.jpg?oh=057868a7f3eb1bf85ce57b70cfb32cb4&oe=597ACB5E'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: 1024,
+      liked: true
+    },
+    {
+      user: {
+        username: 'saulpolo95',
+        avatar: 'https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-1/c0.5.160.160/p160x160/13872993_174638556288372_5069571747674313849_n.jpg?oh=057868a7f3eb1bf85ce57b70cfb32cb4&oe=597ACB5E'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: 1024,
+      liked: true
+    }
+];
+  empty(main).appendChild(template(pictures));
 })
